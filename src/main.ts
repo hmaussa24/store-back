38b7e619eed3761a7e8d927042ab9a37ebcdf0f6
Harold Maussa Rivas <hmaussa.stefanini@bicevida.cl>
2025-05-26 15:11:17 -0500

    /api

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  app.setGlobalPrefix('api');
   await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000/api');
+  console.log('Server is ready to accept requests.');
 }
 bootstrap();
